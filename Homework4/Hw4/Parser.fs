dec929fea42c5a55b5fb47b4dc84ac4fffe4bf9a
NailZinn <nailzinnurov17@gmail.com>
2022-10-05 23:16:18 +0300

    hw4 done

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -3,18 +3,32 @@
 open System
 open Hw4.Calculator
 
-
 type CalcOptions = {
     arg1: float
     arg2: float
     operation: CalculatorOperation
 }
 
-let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+let assertLength (args : string[]) =
+    match args.Length with
+    | 3 -> printf "ok"
+    | _ -> ArgumentException() |> raise
 
 let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> ArgumentException("Could not convert given value to an operation") |> raise
     
 let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    assertLength args
+    try
+        let val1 = Double.Parse(args[0])
+        let val2 = Double.Parse(args[2])
+        let operation = parseOperation args[1]
+        
+        { arg1 = val1; arg2 = val2; operation = operation }
+    with
+        | :? FormatException -> ArgumentException("Could not convert given value to a number") |> raise
