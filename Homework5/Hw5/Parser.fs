95e3afb6a2acbadb83b31bf0cb40f2fbbafa5009
NailZinn <nailzinnurov17@gmail.com>
2022-10-12 12:36:05 +0300

    hw5 added

--- Homework5/Hw5/Parser.fs
+++ Homework5/Hw5/Parser.fs
@@ -2,20 +2,51 @@
 
 open System
 open Hw5.Calculator
+open Hw5.MaybeBuilder
+
+let (|CalculatorOperation|_|) arg =
+    match arg with
+    | Plus -> Some CalculatorOperation.Plus
+    | Minus -> Some CalculatorOperation.Minus
+    | Multiply -> Some CalculatorOperation.Multiply
+    | Divide -> Some CalculatorOperation.Divide
+    | _ -> None
+    
+let (|Double|_|) arg =
+    match Double.TryParse(arg: string) with
+    | true, double -> Some double
+    | _ -> None
 
 let isArgLengthSupported (args:string[]): Result<'a,'b> =
-    (NotImplementedException() |> raise)
+    match args.Length with
+    | 3 -> Ok args
+    | _ -> Error Message.WrongArgLength
     
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let inline isOperationSupported (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)
+    match operation with
+    | CalculatorOperation operation -> Ok (arg1, operation, arg2)
+    | _ -> Error Message.WrongArgFormatOperation
 
-let parseArgs (args: string[]): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)    
+let parseArgs (args: string[]): Result<('a * string * 'b), Message> =
+    match args[0] with
+    | Double val1 ->
+        match args[2] with
+        | Double val2 -> Ok (val1, args[1], val2)
+        | _ -> Error Message.WrongArgFormat
+    | _ -> Error Message.WrongArgFormat
 
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let inline isDividingByZero (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)       
+    match arg2 = 0.0 && operation = CalculatorOperation.Divide with
+    | true -> Error Message.DivideByZero
+    | false -> Ok (arg1, operation, arg2)
     
 let parseCalcArguments (args: string[]): Result<'a, 'b> =
-    (NotImplementedException() |> raise)    
+    maybe {
+        let! argsWithCorrectLength = isArgLengthSupported args
+        let! argsWithCorrectValues = parseArgs argsWithCorrectLength
+        let! argsWithCorrectValuesAndOperation = isOperationSupported argsWithCorrectValues
+        let! noDivisionWithVal2EqualsToZero = isDividingByZero argsWithCorrectValuesAndOperation
+        return noDivisionWithVal2EqualsToZero
+    }
