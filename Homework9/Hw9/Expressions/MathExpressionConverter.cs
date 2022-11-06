eea44a439d79eaa0fd0ba67a76d86401dfcffe56
NailZinn <nailzinnurov17@gmail.com>
2022-11-06 13:40:49 +0300

    hw9 added

---
+++ Homework9/Hw9/Expressions/MathExpressionConverter.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+
+namespace Hw9.Expressions
+{
+    public class MathExpressionConverter
+    {
+        private readonly Dictionary<Expression, Expression[]> executeBefore = new();
+
+        public Dictionary<Expression, Expression[]> ToDictionary(Expression tree)
+        {
+            Visit(tree);
+            return executeBefore;
+        }
+
+        private void Visit(Expression node)
+        {
+            if (node is BinaryExpression binary)
+                VisitBinary(binary);
+            else if (node is UnaryExpression unary)
+                VisitUnary(unary);
+            else
+                VisitConstant((ConstantExpression)node);
+        }
+
+        private void VisitBinary(BinaryExpression node)
+        {
+            if (!executeBefore.ContainsKey(node))
+            {
+                executeBefore.Add(node, new[] { node.Left, node.Right });
+            }
+
+            Visit(node.Left);
+            Visit(node.Right);
+        }
+
+        private void VisitUnary(UnaryExpression node)
+        {
+            if (!executeBefore.ContainsKey(node))
+            {
+                executeBefore.Add(node, new[] { node.Operand });
+            }
+
+            Visit(node.Operand);
+        }
+
+        private void VisitConstant(ConstantExpression node)
+        {
+            if (!executeBefore.ContainsKey(node))
+            {
+                executeBefore.Add(node, new Expression[0]);
+            }
+        }
+    }
+}
--- Homework9/Hw9/Services/MathCalculator/MathCalculatorService.cs
+++ Homework9/Hw9/Services/MathCalculator/MathCalculatorService.cs
@@ -1,4 +1,7 @@
 using Hw9.Dto;
+using Hw9.Expressions;
+using Hw9.Tokens.Math;
+using System.Linq.Expressions;
 
 namespace Hw9.Services.MathCalculator;
 
@@ -6,6 +9,27 @@ public class MathCalculatorService : IMathCalculatorService
 {
     public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        Expression tree = null;
+
+        try
+        {
+            tree = await Task.Run(() => Parser.Parse(expression));
+        }
+        catch (ArgumentException ex)
+        {
+            return new CalculationMathExpressionResultDto(ex.Message);
+        }
+
+        var executeBefore = await Task.Run(() => new MathExpressionConverter().ToDictionary(tree));
+
+        try
+        {
+            var result = await MathExpressionCalculator.CalculateAsync(executeBefore);
+            return new CalculationMathExpressionResultDto(result);
+        }
+        catch (DivideByZeroException ex)
+        {
+            return new CalculationMathExpressionResultDto(ex.Message);
+        }
     }
 }
