8d4da10977c8e398b080a3bd63d61cba5956e91b
NailZinn <nailzinnurov17@gmail.com>
2022-11-27 13:41:09 +0300

    removed todo

--- Homework12/Hw12/TestApplicationFactoryFSharp.cs
+++ Homework12/Hw12/TestApplicationFactoryFSharp.cs
@@ -1,8 +1,8 @@
+using Hw6;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Hw12;
 
-public class TestApplicationFactoryFSharp : WebApplicationFactory<Hw6.App.Startup>
-    // TODO: replace generic argument with the right one
+public class TestApplicationFactoryFSharp : WebApplicationFactory<App.Startup>
 {
 }
